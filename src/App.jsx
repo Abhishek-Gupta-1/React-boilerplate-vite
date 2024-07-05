dbabfb54413b9f446486221513577d3186f1c01d
Abhishek Gupta <abhisheknew2023@gmail.com>
2024-07-06 00:56:01 +0530

    delete some line

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <div><h1 className='text-green-600 text-5xl'>This app is running</h1></div>
